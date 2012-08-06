6e29290b17161580068a49277480187fcb7dcccb
Dan Matthews <dan@hydrant.co.uk>
2012-08-06 17:12:06 +0100

    today's changes including moving around pages into better tabs

--- sp-support-page.tpl.php
+++ sp-support-page.tpl.php
@@ -9,16 +9,3 @@
 
 <h2>Currently open support tickets:</h2>
 <?php print $ticket_table; ?>
-
-<h2>Available knowledge bases:</h2>
-
-
-<?php if (count($knowledge_bases) > 0) : ?>
-	<ul>
-	<?php foreach($knowledge_bases as $knowledge) : ?>
-
-		<li><?php print l($knowledge[1], 'admin/sirportly/knowledge/view/' . $knowledge[0]) ?></li>
-
-	<?php endforeach; ?>
-	</ul>
-<?php endif; ?>
